7b7c03bc3368cd709f69848dd89a8f58f8f1789e
Alien178 <aayushak178@gmail.com>
2021-01-14 17:17:01 -0800

    Added Working Submit Button

--- screens/WriteStoryScreen.js
+++ screens/WriteStoryScreen.js
@@ -1,31 +1,90 @@
 import * as React from "react";
-import { View, Text, StyleSheet, TouchableOpacity, TextInput } from "react-native";
-import { Header } from 'react-native-elements';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  TextInput,
+} from "react-native";
+import { Header } from "react-native-elements";
+import db from "../config";
+import firebase from "firebase";
 
 export default class WriteStoryScreen extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      author: "",
+      title: "",
+      story: "",
+    };
+  }
+
+  submitStory = async () => {
+    db.collection("stories").add({
+      title: this.state.title,
+      author: this.state.author,
+      story: this.state.story,
+      date: firebase.firestore.Timestamp.now().toDate(),
+    });
+  };
+
   render() {
     return (
-        <View style={styles.container}>
-            <Header
-            centerComponent={{
-                text: 'Writing Hub',
-                style: { color: 'white', fontSize: 20, fontWeight: 'bold' },
+      <View style={styles.container}>
+        <Header
+          centerComponent={{
+            text: "Writing Hub",
+            style: { color: "white", fontSize: 20, fontWeight: "bold" },
+          }}
+          backgroundColor={"purple"}
+        />
+        <View>
+          <TextInput
+            style={styles.titleInputBox}
+            placeholder="Title of the Story"
+            onChangeText={(text) => {
+              this.setState({
+                title: text,
+              });
             }}
-            backgroundColor={'purple'}
-            />
-            <View>
-                <TextInput style = {styles.titleInputBox} placeholder = "Title of the Story"/>
-            </View>
-            <View>
-                <TextInput style = {styles.authorInputBox} placeholder = "Name of the Author"/>
-            </View>
-            <View>
-                <TextInput style = {styles.storyInputBox} multiline numberOfLines = {30} placeholder = "Write your Story Here"/>
-            </View>
-            <View>
-                <TouchableOpacity style = {styles.sendButton}><Text style = {styles.sendButtonText}>Send Story</Text></TouchableOpacity>
-            </View>
+          />
         </View>
+        <View>
+          <TextInput
+            style={styles.authorInputBox}
+            placeholder="Name of the Author"
+            onChangeText={(text) => {
+              this.setState({
+                author: text,
+              });
+            }}
+          />
+        </View>
+        <View>
+          <TextInput
+            style={styles.storyInputBox}
+            multiline
+            numberOfLines={30}
+            placeholder="Write your Story Here"
+            onChangeText={(text) => {
+              this.setState({
+                story: text,
+              });
+            }}
+          />
+        </View>
+        <View>
+          <TouchableOpacity
+            style={styles.sendButton}
+            onPress={() => {
+              this.submitStory();
+            }}
+          >
+            <Text style={styles.sendButtonText}>Send Story</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
     );
   }
 }
@@ -39,52 +98,52 @@ const styles = StyleSheet.create({
     marginTop: 15,
     width: 350,
     borderWidth: 4,
-    alignSelf: 'center',
+    alignSelf: "center",
     height: 50,
-    textAlign: 'center',
+    textAlign: "center",
     borderRadius: 5,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     fontSize: 24,
   },
   authorInputBox: {
     marginTop: 15,
     width: 350,
     borderWidth: 4,
-    alignSelf: 'center',
+    alignSelf: "center",
     height: 50,
-    textAlign: 'center',
+    textAlign: "center",
     borderRadius: 5,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     fontSize: 20,
   },
   storyInputBox: {
     marginTop: 15,
     width: 350,
     borderWidth: 4,
-    alignSelf: 'center',
+    alignSelf: "center",
     height: 350,
     borderRadius: 5,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     fontSize: 17,
     textAlignVertical: "top",
-    textAlign: 'auto',
+    textAlign: "auto",
     paddingHorizontal: 5,
   },
   sendButton: {
     marginTop: 15,
     width: 150,
     borderWidth: 3,
-    alignSelf: 'center',
+    alignSelf: "center",
     height: 50,
     borderRadius: 10,
-    backgroundColor: '#009604',
-    alignItems: 'center',
-    justifyContent: 'center',
-    borderColor: '#005604',
+    backgroundColor: "#009604",
+    alignItems: "center",
+    justifyContent: "center",
+    borderColor: "#005604",
   },
 
   sendButtonText: {
-    fontWeight: 'bold',
+    fontWeight: "bold",
     fontSize: 20,
-  }
+  },
 });
